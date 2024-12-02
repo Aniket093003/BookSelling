75f12423bf7fedf0a90a259d4442b68a50382300
Aniket093003 <aniketkainth98105@gmail.com>
2024-12-03 00:41:27 +0530

    put endpoint

--- Backend/route/book.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getBook } from "../controller/book.controller.js";
-
-const bookRoute = express.Router();
-
-bookRoute.get("/", getBook);
-
-export default bookRoute;
--- Backend/route/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signup, login } from "../controller/user.controller.js";
-const userRoute = express.Router();
-
-userRoute.post("/signup", signup);
-userRoute.post("/login", login);
-
-export default userRoute;
